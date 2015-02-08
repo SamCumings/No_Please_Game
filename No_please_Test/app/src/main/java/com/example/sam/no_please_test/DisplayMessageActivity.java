06994f426e36c76ce7659bb6d6c9cb1e0c2006db
thedsm <thedsm@gmail.com>
2015-02-08 13:06:24 -0600

    More tutorial
    2nd activity

--- No_please_Test/app/src/main/java/com/example/sam/no_please_test/DisplayMessageActivity.java
+++ No_please_Test/app/src/main/java/com/example/sam/no_please_test/DisplayMessageActivity.java
@@ -4,25 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    public final static String EXTRA_MESSAGE = "com.example.sam.no_please_test.MESSAGE";
+public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_message);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
         return true;
     }
 
@@ -39,14 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view){
-        //do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
