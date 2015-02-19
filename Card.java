52bb37e15f9c6c50d25e7b88af5b655ec0c1d325
Kymberley Weston <weston.kymberley@gmail.com>
2015-02-19 15:47:29 -0600

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,37 @@
+ public class Card 
+ {
+	private int id;
+	private Bitmap bmp;
+	private numTokensAttatched = 0;
+	
+	public Card(int newId) 
+	{
+		id = newId;
+	}
+
+	public void setBitmap(Bitmap newBitmap) 
+	{
+		bmp = newBitmap; 
+	}
+	
+	public Bitmap getBitmap() 
+	{
+		return bmp;
+	}
+	
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public void attatchToken()
+	{
+		numTokensAttatched += numTokensAttatched
+	}
+	
+	
+
+}
+
+
+
