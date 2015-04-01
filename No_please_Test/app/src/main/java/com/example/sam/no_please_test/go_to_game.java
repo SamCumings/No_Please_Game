1db236f55898fae424b369886ca62088bd016c93
Sam Cumings <thedsm@gmail.com>
2015-04-01 13:47:26 -0500

    Now we are initializing the game screen to the default game state. Need to do more testing but at least it does a little.
    (I think that's what I'm commiting)

--- No_please_Test/app/src/main/java/com/example/sam/no_please_test/go_to_game.java
+++ No_please_Test/app/src/main/java/com/example/sam/no_please_test/go_to_game.java
@@ -8,8 +8,46 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 
 public class go_to_game extends ActionBarActivity {
+    //handles issues of displaying the game
+
+    public void update(Game new_game){
+        TextView current_card = (TextView) this.findViewById(R.id.Current_Card);
+        current_card.setText(String.valueOf(new_game.current_card.getValue()));
+
+        TextView current_card_chips = (TextView) this.findViewById(R.id.Current_Card_Chips);
+        current_card_chips.setText(String.valueOf(new_game.current_card.numChips));
+
+        TextView current_player_name = (TextView) this.findViewById(R.id.current_player_name);
+        current_player_name.setText(String.valueOf(new_game.Current_Player.playerName));
+
+        TextView current_player_tokens = (TextView) this.findViewById(R.id.current_player_chip_count);
+        current_player_tokens.setText(String.valueOf(new_game.Current_Player.tokenCount));
+
+        TextView player_1_cards = (TextView) this.findViewById(R.id.Player1_cards);
+        player_1_cards.setText(Arrays.toString(new_game.Player_List[0].Hand));
+
+        TextView player_2_cards = (TextView) this.findViewById(R.id.Player2_cards);
+        player_2_cards.setText(Arrays.toString(new_game.Player_List[1].Hand));
+
+        TextView player_3_cards = (TextView) this.findViewById(R.id.Player3_cards);
+        player_3_cards.setText(Arrays.toString(new_game.Player_List[2].Hand));
+
+        if(new_game.number_players>3) {
+            TextView player_4_cards = (TextView) this.findViewById(R.id.Player4_cards);
+            player_4_cards.setText(Arrays.toString(new_game.Player_List[3].Hand));
+        }
+        if(new_game.number_players>4){
+            TextView player_5_cards = (TextView) this.findViewById(R.id.Player5_cards);
+            player_5_cards.setText(Arrays.toString(new_game.Player_List[4].Hand));
+        }
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,16 +85,43 @@ public class go_to_game extends ActionBarActivity {
         TextView player5 = (TextView) this.findViewById(R.id.Player5);
         player5.setText(String.valueOf(player_5_name));
 
+        //game called
         Game new_game = new Game (number_players,player_1_name,player_2_name,player_3_name,player_4_name,player_5_name);
 
+        //hides unused textviews (if statements for players 4 and 5)
+
+        //update function pretty much
         TextView current_card = (TextView) this.findViewById(R.id.Current_Card);
         current_card.setText(String.valueOf(new_game.current_card.getValue()));
+
+        TextView current_card_chips = (TextView) this.findViewById(R.id.Current_Card_Chips);
+        current_card_chips.setText(String.valueOf(new_game.current_card.numChips));
 
         TextView current_player_name = (TextView) this.findViewById(R.id.current_player_name);
         current_player_name.setText(String.valueOf(new_game.Current_Player.playerName));
 
         TextView current_player_tokens = (TextView) this.findViewById(R.id.current_player_chip_count);
         current_player_tokens.setText(String.valueOf(new_game.Current_Player.tokenCount));
+
+        TextView player_1_cards = (TextView) this.findViewById(R.id.Player1_cards);
+        player_1_cards.setText(Arrays.toString(new_game.Player_List[0].Hand));
+
+        TextView player_2_cards = (TextView) this.findViewById(R.id.Player2_cards);
+        player_2_cards.setText(Arrays.toString(new_game.Player_List[1].Hand));
+
+        TextView player_3_cards = (TextView) this.findViewById(R.id.Player3_cards);
+        player_3_cards.setText(Arrays.toString(new_game.Player_List[2].Hand));
+
+        if(number_players>3) {
+            TextView player_4_cards = (TextView) this.findViewById(R.id.Player4_cards);
+            player_4_cards.setText(Arrays.toString(new_game.Player_List[3].Hand));
+        }
+        if(number_players>4){
+            TextView player_5_cards = (TextView) this.findViewById(R.id.Player5_cards);
+            player_5_cards.setText(Arrays.toString(new_game.Player_List[4].Hand));
+        }
+
+
 
 
     }
@@ -100,6 +165,8 @@ public class go_to_game extends ActionBarActivity {
             //change current amount of chips (to the new current player)
             //change current highlighted player
             //check if the pass button should be greyed out for this player
+
+
 
     }
     //called when pass button pressed
