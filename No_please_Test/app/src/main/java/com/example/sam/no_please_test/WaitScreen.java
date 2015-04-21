49ed1b643ccda72d7535cd05604149e738d714ec
ejt0062 <zombiepiss@gmail.com>
2015-04-21 03:08:48 -0500

    Trashed the Fragments since they were not needed and used an Activity to make a Wait Screen instead.  Have a partially working Waiting Screen.

---
+++ No_please_Test/app/src/main/java/com/example/sam/no_please_test/WaitScreen.java
@@ -0,0 +1,16 @@
+package com.example.sam.no_please_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by TJ on 4/21/2015.
+ */
+public class WaitScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waitscreen);
+    }
+}
--- No_please_Test/app/src/main/java/com/example/sam/no_please_test/go_to_game.java
+++ No_please_Test/app/src/main/java/com/example/sam/no_please_test/go_to_game.java
@@ -1,11 +1,14 @@
 package com.example.sam.no_please_test;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -77,6 +80,7 @@ public class go_to_game extends ActionBarActivity {
     }
 
     Game new_game;
+
     Card test_card;
 
 
@@ -84,6 +88,12 @@ public class go_to_game extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
+
+        //FragmentManager FM = getFragmentManager();
+        //FragmentTransaction FT = FM.beginTransaction();
+        //WaitScreenFragment waitScreenFragment = new WaitScreenFragment();
+        //FT.add(R.id., waitScreenFragment);
+        //FT.commit();
 
         //this gets the number of players (doesn't get anything else yet that's what the commented out code is for).
         Bundle bundle = getIntent().getExtras();
@@ -165,7 +175,11 @@ public class go_to_game extends ActionBarActivity {
             //change current highlighted player
             //check if the pass button should be greyed out for this player
 
+
+        Intent intent = new Intent(this, WaitScreen.class);
+
         test_card=new_game.take_card();
+        startActivity(intent);
         update(new_game);
 
 
